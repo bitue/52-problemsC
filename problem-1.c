5d2df964775aaecafbca9ad43721f733bf234857
bitue <ashikul.islam_ashik@yahoo.com>
2022-03-15 23:55:12 +0600

    first-problem-done

---
+++ problem-1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void main (){
+
+    int T , i , n ;
+
+    scanf("%d", &T);
+
+    for(i =0 ; i< T ; i++) {
+        int input ;
+        scanf("%d", &input) ;
+        if(input%2 == 0) {
+            printf("even \n");
+        }
+        else {
+             printf("odd \n");
+        }
+    }
+
+
+
+
+
+}
