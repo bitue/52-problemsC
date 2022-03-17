fe11260100e168a685abec472925b12fa4a7acaf
bitue <ashikul.islam_ashik@yahoo.com>
2022-03-18 01:15:04 +0600

    2 and 3

--- problem2.c
+++ problem2.c
@@ -1,21 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
-void main(){
 
-    int T ,i ,n ;
 
-    scanf("%d", &T);
-    for(i=1 ; i<=T ; i++){
-        scanf("%d", &n);
-        if(n%2 ==0){
+
+void main (){
+    int n ;
+    scanf("%d", &n);
+    char long_num[101];
+
+    for(int i=0 ; i<n ; i++) {
+        scanf("%s", &long_num);
+        // now need the last digit of the chars and then follow it that it is whether even or odd
+        int len = strlen(long_num);
+        if(long_num[len-1]%2 ==0) {
             printf("even \n");
         }
         else {
             printf("odd \n");
         }
+
     }
-
-
-
 
 }
---
+++ problem4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+void main () {
+
+    int T ;
+    int n ;
+
+    scanf("%d", &T);
+
+    for(int i=0 ; i<T ; i++) {
+
+        scanf(%d , &n );
+
+        for(int j=2 ; j<=n ; j++) {
+
+        }
+    }
+
+
+
+}
