8d7631b68ae59d5202f3b0e6816e643fdcda431a
bitue <ashikul.islam_ashik@yahoo.com>
2022-03-21 23:40:52 +0600

    10

---
+++ problem10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main (){
+
+    int T ;
+    scanf("%d", &T);
+
+    for(int i =0 ; i<T; i++){
+        int run, run_target , balls ;
+        double curr_run_rate , req_run_rate ;
+
+
+        scanf("%d %d %d", run_target, run, balls);
+        run_target ++ ;
+        int ball_remain = 300 - balls ;
+        req_run_rate = 6*((run_target - run ) / ball_remain ) ;
+        req_run_rate = 6*()
+
+
+    }
+
+
+
+}
--- problem2.c
+++ problem2.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-
-
-
-
 void main (){
     int n ;
     scanf("%d", &n);
